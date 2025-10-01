ab4a1bfb0e57ab8d7f44952f1c38b8ebf57c246c
x0rtex <s00253368@atu.ie>
2025-10-01 22:40:51 +0100

    Created db models module

---
+++ dobrydo/models/task.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+import datetime
+
+
+@dataclass
+class Task:
+    id: int
+    title: str
+    content: str | None
+    due_date: datetime.date | None
+    created_at: datetime.datetime
+    completed_at: datetime.datetime | None
+    tags: list[str] | None
