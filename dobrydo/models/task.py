07c374ea95536d01aa2edf9b69aba845a68144d0
x0rtex <s00253368@atu.ie>
2025-10-02 08:48:59 +0100

    Removed other models from task model

--- dobrydo/models/task.py
+++ dobrydo/models/task.py
@@ -11,21 +11,3 @@ class Task:
     created_at: datetime.datetime
     completed_at: datetime.datetime | None
     tags: list[str] | None
-
-
-@dataclass
-class Note:
-    id: int
-    title: str
-    content: str | None
-    created_at: datetime.datetime
-    tags: list[str] | None
-
-
-@dataclass
-class Timer:
-    id: int
-    title: str
-    duration: datetime.timedelta
-    created_at: datetime.datetime
-    completed_at: datetime.datetime | None
